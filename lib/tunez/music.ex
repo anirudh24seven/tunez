37fddb9670a3471537df3392bc8b3961932b287f
Anirudh S <anirudh24seven@gmail.com>
2025-04-16 22:49:28 +0530

    Some changes

---
+++ lib/tunez/music.ex
@@ -0,0 +1,7 @@
+defmodule Tunez.Music do
+  use Ash.Domain, otp_app: :tunez
+
+  resources do
+    resource Tunez.Music.Artist
+  end
+end
---
+++ lib/tunez/music/artist.ex
@@ -0,0 +1,21 @@
+defmodule Tunez.Music.Artist do
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+    end
+
+    attribute :biography, :string
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+
+  postgres do
+    table "artists"
+    repo Tunez.Repo
+  end
+end
