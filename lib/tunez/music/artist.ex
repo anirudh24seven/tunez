b043662a9a83bba61199e09f93d6e18d349be57b
Anirudh S <anirudh24seven@gmail.com>
2025-04-17 01:36:57 +0530

    Inline change

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -10,19 +10,42 @@ defmodule Tunez.Music.Artist do
 
     attribute :biography, :string
 
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
+
     create_timestamp :inserted_at
     update_timestamp :updated_at
   end
 
   actions do
-    defaults [:create, :read, :update, :destroy]
-    default_accept [:name, :biography]
-  end
-
-  relationships do
-    has_many :albums, Tunez.Music.Album do
-      sort year_released: :desc
+    create :create do
+      accept [:name, :biography]
     end
+
+    read :read do
+      primary? true
+    end
+
+    update :update do
+      require_atomic? false
+      accept [:name, :biography]
+
+      change fn changeset, _context ->
+        new_name = Ash.Changeset.get_attribute(changeset, :name)
+        previous_name = Ash.Changeset.get_data(changeset, :name)
+        previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+
+        names = [previous_name | previous_names]
+          |> Enum.uniq()
+          |> Enum.reject(fn name -> name == new_name end)
+
+        Ash.Changeset.change_attribute(changeset, :previous_names, names)
+      end,
+        where: [changing(:name)]
+    end
+
+    destroy :destroy
   end
 
   postgres do
