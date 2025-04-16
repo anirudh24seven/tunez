a65452672eb05f74f4323fe47f6e322032f82f35
Anirudh S <anirudh24seven@gmail.com>
2025-04-17 01:00:08 +0530

    Add simple validations

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -36,6 +36,16 @@ defmodule Tunez.Music.Album do
     end
   end
 
+  validations do
+    validate numericality(:year_released, greater_than: 1950, less_than_or_equal_to: &__MODULE__.next_year/0),
+      where: [present(:year_released)],
+      message: "must be between 1950 and next year"
+
+    validate match(:cover_image_url, ~r"(^https://|/images/).+(\.png|\.jpg)$"),
+      where: [changing(:cover_image_url)],
+      message: "must start with https:// or /images/"
+  end
+
   postgres do
     table "albums"
     repo Tunez.Repo
@@ -43,5 +53,9 @@ defmodule Tunez.Music.Album do
     references do
       reference :artist, index?: true
     end
+  end
+
+  def next_year do
+    Date.utc_today().year + 1
   end
 end
