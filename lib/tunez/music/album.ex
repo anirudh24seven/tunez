f83d5c006d8143fc395d3d5a9df7d65d2e7b8e71
Anirudh S <anirudh24seven@gmail.com>
2025-04-18 18:33:26 +0530

    API for showing albums of a given artist

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -1,21 +1,21 @@
 defmodule Tunez.Music.Album do
-  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    otp_app: :tunez,
+    domain: Tunez.Music,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshJsonApi.Resource]
 
-  attributes do
-    uuid_primary_key :id
+  json_api do
+    type "album"
+  end
 
-    attribute :name, :string do
-      allow_nil? false
+  postgres do
+    table "albums"
+    repo Tunez.Repo
+
+    references do
+      reference :artist, index?: true, on_delete: :delete
     end
-
-    attribute :year_released, :integer do
-      allow_nil? false
-    end
-
-    attribute :cover_image_url, :string
-
-    create_timestamp :inserted_at
-    update_timestamp :updated_at
   end
 
   actions do
@@ -30,24 +30,44 @@ defmodule Tunez.Music.Album do
     end
   end
 
-  relationships do
-    belongs_to :artist, Tunez.Music.Artist do
-      allow_nil? false
-    end
-  end
-
   validations do
-    validate numericality(:year_released, greater_than: 1950, less_than_or_equal_to: &__MODULE__.next_year/0),
-      where: [present(:year_released)],
-      message: "must be between 1950 and next year"
+    validate numericality(:year_released,
+               greater_than: 1950,
+               less_than_or_equal_to: &__MODULE__.next_year/0
+             ),
+             where: [present(:year_released)],
+             message: "must be between 1950 and next year"
 
     validate match(:cover_image_url, ~r"(^https://|/images/).+(\.png|\.jpg)$"),
       where: [changing(:cover_image_url)],
       message: "must start with https:// or /images/"
   end
 
-  identities do
-    identity :unique_album_names_per_artist, [:name, :artist_id], message: "already exists for this artist"
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string do
+      allow_nil? false
+      public? true
+    end
+
+    attribute :year_released, :integer do
+      allow_nil? false
+      public? true
+    end
+
+    attribute :cover_image_url, :string do
+      public? true
+    end
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+
+  relationships do
+    belongs_to :artist, Tunez.Music.Artist do
+      allow_nil? false
+    end
   end
 
   calculations do
@@ -55,16 +75,12 @@ defmodule Tunez.Music.Album do
     calculate :string_years_ago, :string, expr("released" <> years_ago <> "years ago")
   end
 
-  postgres do
-    table "albums"
-    repo Tunez.Repo
-
-    references do
-      reference :artist, index?: true, on_delete: :delete
-    end
-  end
-
   def next_year do
     Date.utc_today().year + 1
+  end
+
+  identities do
+    identity :unique_album_names_per_artist, [:name, :artist_id],
+      message: "already exists for this artist"
   end
 end
